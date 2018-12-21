038e735ac37d50942546b10f6516d1a419c54392
meet <gadoya@usc.edu>
2018-12-21 00:32:44 -0800

    ascii to nm

--- problem168.py
+++ problem168.py
@@ -1,9 +1,23 @@
-def convertToTitle(self, num):
-        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
-        result = []
-        while num > 0:
-            result.append(capitals[(num-1)%26])
-            num = (num-1) // 26
-        result.reverse()
-        return ''.join(result)
+def titleToNumber(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    d=[x-64 for x in range(ord('A'),ord('Z')+1)]
+    print(d)
+    c=0
+    i=0
+    for a in reversed(s):
+        m=ord(a)-65 #ASCII
+        if c==0:
+            c+=d[m]
+            i=i+1
+        else:
+            c+=d[m]*pow(26,i)
+            i=i+1
+        #c+=(d[ord(a)-65]+1)+i*26
+        #print(c)
+        #i=i+1
+        print(c)
+    return c
 

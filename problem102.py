12e376f5d802ce289e6092ca93d5feac307afcdc
meet <gadoya@usc.edu>
2018-12-22 04:25:11 -0800

    duplicate in array

---
+++ problem102.py
@@ -0,0 +1,10 @@
+if not root:
+    return []
+ans, level = [], [root]
+while level:
+    ans.append([node.val for node in level])
+    temp = []
+    for node in level:
+        temp.extend([node.left, node.right])
+    level = [leaf for leaf in temp if leaf]
+return ans

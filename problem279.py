cd4ee61f44763766862e218682075ae1b2605fc0
meet <gadoya@usc.edu>
2018-12-29 04:26:41 -0800

    Perfect Squares

---
+++ problem279.py
@@ -0,0 +1,22 @@
+if n < 2:
+    return n
+lst = []
+i = 1
+while i * i <= n:
+    lst.append( i * i )
+    i += 1
+cnt = 0
+toCheck = [n]
+while toCheck:
+    cnt += 1
+    temp = []
+    for x in toCheck:
+        for y in lst:
+            if x == y:
+                return cnt
+            if x < y:
+                break
+            temp.append(x-y)
+    toCheck = temp
+
+return cnt

aedfa433c54c4d8713ac25e8f8e8acb170943716
meet <gadoya@usc.edu>
2018-12-29 21:09:39 -0800

    Number of islands

--- problem200.py
+++ problem200.py
@@ -1,46 +1,34 @@
-def numIslands(grid):
-    a=len(grid)
-    b=len(grid[0])
-    #print((a,b))
-    c=0             #Count
-    global v
-    v=[[0]*b for i in range(a)]    #Visited
-    #print(v)
-    s=[]            #States
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            s.append((i,j))
-    i=j=0
-    #print(s)
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        a = len(grid)
+        b = len(grid[0])
+        c = 0  # Count
 
+        global v
+        v = [[0] * b for i in range(a)]  # Visited
 
-    while(i<a):
-        j=0
-        while(j<b):
-            if (int(grid[i][j])==1 and v[i][j]==0):
-               
-                helper(s,grid, c, i, j)
-                #print(v)
-                c=c+1
-            j=j+1
-        i=i+1
+        i = j = 0
+        while (i < a):
+            j = 0
+            while (j < b):
+                if (int(grid[i][j]) == 1 and v[i][j] == 0):
+                    self.helper(grid, i, j)
+                    # print(v)
+                    c = c + 1
+                j = j + 1
+            i = i + 1
 
-    #v[2][2]=1
-    #print(v)
-    return c
+        return c
 
+    def helper(self, grid, i, j):
+        global v
 
-def helper(s,grid,c,i,j):
-    global v
-    if (i,j) in s:
-        #print((i,j))
-        if((int(grid[i][j])==1) and (v[i][j]==0)):
-            #print(v,i,j)
-            v[i][j]=1
-            helper(s,grid,c,i+1,j)
-            helper(s,grid, c, i-1 , j)
-            helper(s,grid, c, i, j-1)
-            helper(s,grid, c, i , j+1)
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0' or v[i][j]:
+            return
+        v[i][j] = 1
+        self.helper(grid, i + 1, j)
+        self.helper(grid, i - 1, j)
+        self.helper(grid, i, j - 1)
+        self.helper(grid, i, j + 1)
 
-
-print(numIslands([["0","1","0"],["1","0","1"],["0","1","0"]]))

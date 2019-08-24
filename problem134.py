1aa96ce64c7736af0b2bb7dacf2a68db0d38304b
meetgadoya <gadoya@usc.edu>
2019-08-24 10:03:23 -0700

    Gas Station

---
+++ problem134.py
@@ -0,0 +1,82 @@
+'''Note:
+
+    If there exists a solution, it is guaranteed to be unique.
+    Both input arrays are non-empty and have the same length.
+    Each element in the input arrays is a non-negative integer.
+
+Example 1:
+
+Input:
+gas  = [1,2,3,4,5]
+cost = [3,4,5,1,2]
+
+Output: 3
+
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
+
+Example 2:
+
+Input:
+gas  = [2,3,4]
+cost = [3,4,3]
+
+Output: -1
+
+Explanation:
+You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 0. Your tank = 4 - 3 + 2 = 3
+Travel to station 1. Your tank = 3 - 3 + 3 = 3
+You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+Therefore, you can't travel around the circuit once no matter where you start.
+'''
+
+# my solution, passes all the test case
+#logic: runtime = 2472 ms beats only 5% of other python code.
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        n = len(gas)
+
+        for i in range(n):
+            if gas[i] >= cost[i]:
+                tank = gas[i] - cost[i]
+                next_stn = (i + 1) % n
+                flag = 1
+                while next_stn != i:
+                    tank = tank + gas[next_stn] - cost[next_stn]
+                    if tank < 0:
+                        flag = 0
+                        break
+                    next_stn = (next_stn + 1) % n
+                if flag == 1:
+                    return i
+        return -1
+
+########################################################################################################################
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        gas_left = gas_needed = start = 0
+        for i, (g, c) in enumerate(zip(gas, cost)):
+            gas_left += g - c
+            if gas_left < 0:
+                gas_needed -= gas_left
+                start = i + 1
+                gas_left = 0
+        return start if gas_left >= gas_needed else -1

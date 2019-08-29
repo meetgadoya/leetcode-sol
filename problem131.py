b9435ab7c6c505302151d03f9661d09fe1668b97
meetgadoya <gadoya@usc.edu>
2019-08-28 18:09:51 -0700

    Palindrome Partitioning

--- problem131.py
+++ problem131.py
@@ -1,62 +1,24 @@
-def partition(s):
-    k = [s]
-    res=[]
-    c=0
-    while c<2:
-        k=helper(k)
-        res.append(k)
-        print("In main L",k)
-        #res.insert(-1,L)
-        print("RES",res)
-        c+=1
-        # for l in L:
-        #     if len(l)>1:
-        #         c=0
 
-    print("2nd Time")
+#logic : runtime = 72ms beats 65%
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
 
-    print("In main",res )
+        res = []
 
-def helper(L):
-    temp = []
-    while L:
-        n = L.pop(0)
+        def helper(s, path, res):
+            if not s:
+                res.append(path)
+                return
+            for i in range(1, len(s) + 1):
+                if self.pal(s[:i]):
+                    helper(s[i:], path + [s[:i]], res)
 
-        #print(L)
-        if len(n) < 2:
-            temp.extend(n)
-        elif len(n) == 2:
-            temp.extend(n[0])
-            temp.extend(n[1])
-            #print("len==2", temp)
-        else:
-            i = 0
-            while i < len(n):
-                flag = 0
-                for j in range(len(n) - 1, i - 1, -1):
-                    #print(i, j)
-                    # if(n[i]!=n[j]):
-                    #     flag=1
-                    #     break
-                    if (n[i] == n[j]):
-                        # print(i,j)
+        helper(s, [], res)
+        return res
 
-                        for k in range(i + 1, j):
-                            if (n[k] != n[j - k]):
-                                flag = 1
-                                break
-                        if (flag == 1):
-                            break
-                        else:
-                            #print("Hey", i, j, n[i:j + 1])
-                            temp.extend([n[i:j + 1]])
-                            #print("Temp", temp)
-                            i = j
-                            break
-                i += 1
-    L = temp
-    print("Final L", L)
-    return L
-
-
-print(partition("aab"))
+    def pal(self, s):
+        return s == s[::-1]
